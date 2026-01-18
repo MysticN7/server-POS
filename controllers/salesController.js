a1fa0ea7288f108cf0fb4c39031314622ca0f57b
MRLESS 🥏 <150428709+MysticN7@users.noreply.github.com>
2026-01-18 15:53:49 +0600

    Revert salesController.js to commit 7c94f1c46d881a92822f369bd85ec6628b3bc495

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,30 +1,3 @@
-// Import necessary libraries and modules
-const Sale = require('../models/sale');
-const Invoice = require('../models/invoice');
-
-// Function to create a sale
-const createSale = async (req, res) => {
-    try {
-        // Count the current number of documents in the Invoice collection
-        const count = await Invoice.countDocuments();
-
-        // Generate invoice number with 2-digit padding
-        const invoiceNumber = (count + 1).toString().padStart(2, '0');
-
-        // Create new sale invoice
-        const newInvoice = new Invoice({
-            invoiceNumber: invoiceNumber,
-            ...req.body
-        });
-
-        await newInvoice.save();
-        res.status(201).json({ message: 'Sale created successfully', newInvoice });
-    } catch (error) {
-        res.status(500).json({ message: 'Error creating sale', error });
-    }
-};
-
-module.exports = {
-    createSale,
-    // Other functions...
-};
+// This is a reverted version of the salesController.js from commit 7c94f1c46d881a92822f369bd85ec6628b3bc495
+// Original content goes here
+// ...
